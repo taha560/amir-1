e70782ce512e848c54161ffd3c561927ae76bc73
AVENGERS <golmahdi0@gmail.com>
2016-02-05 00:45:22 +0330

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -1,13 +1,13 @@
 do
 
 function run(msg, matches)
-  return "دستور زیر را به پیوی ربات ارسال کنید تا به گروه ساپورت وصل شوید   join 108275521!"
+  return "دستور زیر را به پیوی ربات ارسال کنید تا به گروه ساپورت وصل شوید   join 103661224!"
   end
 return {
   description = "shows persian support", 
-  usage = "!joinnod32 : Return persian supports link",
+  usage = "!joinsu : Return persian supports link",
   patterns = {
-    "^joinnod32$",
+    "^joinsu$",
     "^/joinnod32$",
     "^!joinnod32$",
     "^>joinnod32$",
