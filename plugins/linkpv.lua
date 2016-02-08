c69ab9997c34df944287f39c9db38abe30bf0c4e
AVENGERS <golmahdi0@gmail.com>
2016-02-08 15:02:09 +0330

    Update linkpv.lua

--- plugins/linkpv.lua
+++ plugins/linkpv.lua
@@ -2,20 +2,20 @@ do
 
 function run(msg, matches)
        if not is_momod(msg) then
-        return "For moderators only!"
+        return "فقط ادمین بات"
        end
 	  local data = load_data(_config.moderation.data)
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
        if not group_link then 
-        return "Create a link using /newlink first !"
+        return "این گروه لینکی نداره اول لینک جدید بساز بعد   /newlink"
        end
-         local text = "Group link:\n"..group_link
+         local text = "لینک گروه:\n"..group_link
           send_large_msg('user#id'..msg.from.id, text.."\n", ok_cb, false)
 end
 
 return {
   patterns = {
-    "^[/!]([Ll]inkpv)$"
+    "^[/!]([Ll][iI][nN][Kk][Pp][Vv])$"
   },
   run = run
 }
