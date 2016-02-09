834d94b41b28a674e8cf76d5e4aa8d12ffa30c03
amirmrbad <amirmrbadboy@gmail.com>
2016-02-09 17:49:15 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -252,7 +252,7 @@ function create_config( )
     "left",
     "support",
     },
-    sudo_users = {139534999},--Sudo users
+    sudo_users = {144152859},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
