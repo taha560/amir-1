a31fbf1b33ed2f4b33f05280bc3fbbd28d8dc74b
5620r <tahat8530@gmail.com>
2016-02-11 12:50:21 +0330

    Update share.lua

--- plugins/share.lua
+++ plugins/share.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+63 938 017 0627", "superbot", "superbot", ok_cb, false)
+send_contact(get_receiver(msg), "+6288973995874", "superbot", "superbot", ok_cb, false)
 end
 
 return {
